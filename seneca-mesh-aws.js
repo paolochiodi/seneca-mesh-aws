1b6dda765b58e832a68387f79860d70b6c921b94
Paolo Chiodi <chiodi84@gmail.com>
2016-11-15 14:00:05 +0100

    initial commit

---
+++ seneca-mesh-aws.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const GetIp = require('aws-get-ip')
+const DEFAULT_PORT = 39000
+
+function addPort (port) {
+  return function (ip) {
+    return ip + ':' + port
+  }
+}
+
+function senecaMeshAws (options) {
+
+  const seneca = this
+  const awsOpts = options.aws || {}
+  const tags = options.baseTags || {mesh:'base'}
+
+  delete options.awsOpts
+  delete options.tags
+
+  const finder = new GetIp(awsOpts)
+  finder.byTags(tags, function gotIps (err, ips) {
+    options.base = ips.map(addPort(DEFAULT_PORT))
+    seneca.use('mesh', options)
+  })
+}
+
+module.exports = senecaMeshAws
